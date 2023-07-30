dbda363cff4774186c990b0a69c6d7532f8c248e
MiriamRehema <rehemamiriam30@gmail.com>
2023-07-30 19:38:54 +0300

    react-hooks-jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
-}
 
+function About(){
+ return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a paragraph about me and what I do.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+  };
 export default About;
